82c6e9fd3fa9d72d8b013cebcfdc44909b862e2c
szymel2020 <szymon.chala@gmail.com>
2021-04-26 21:06:24 +0200

    gallery white/black

--- Lesson-11-Second-page.js
+++ Lesson-11-Second-page.js
@@ -1,5 +1,13 @@
 $(function() {
     $("#menu1").click(function() {
         $("#foto1").slideToggle(3500);
+        
+    });
+});
+
+$(function() {
+    $("#menu2").click(function() {
+        $("#foto2").slideToggle(3500);
+        
     });
 });
