947ebd7daf6c9b74bb97a6039e39890eda2fe214
szymel2020 <szymon.chala@gmail.com>
2021-04-27 19:20:32 +0200

    button border-radius

--- Lesson-11-Second-page.js
+++ Lesson-11-Second-page.js
@@ -3,13 +3,13 @@ $(function() {
         $("#foto1").slideToggle(3500);
         
     });
+     $("#menu2").click(function () {
+       $("#foto2").slideToggle(3500);
+     });
 });
 
 $(function() {
-    $("#menu2").click(function() {
-        $("#foto2").slideToggle(3500);
-        
-    });
+   
 });
 
 $(function() {
