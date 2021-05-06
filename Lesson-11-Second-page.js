46227eb7eb3a157293ad464315034f8d8c370e0e
szymel2020 <szymon.chala@gmail.com>
2021-05-06 22:24:15 +0200

    change slideToggle

--- Lesson-11-Second-page.js
+++ Lesson-11-Second-page.js
@@ -3,25 +3,20 @@
         $("#foto1").slideToggle(3500);
         
     });
+   
      $("#menu2").click(function () {
        $("#foto2").slideToggle(3500);
      });
-});
-
-$(function() {
    
+   $("#menu3").click(function () {
+     $("#foto3").slideToggle(3500);
+   });
+
+   $("#menu4").click(function () {
+     $("#foto4").slideToggle(3500);
+   });
+
 });
 
-$(function() {
-    $("#menu3").click(function() {
-        $("#foto3").slideToggle(3500);
-        
-    });
-});
 
-$(function () {
-  $("#menu4").click(function () {
-    $("#foto4").slideToggle(3500);
-  });
-});
 
